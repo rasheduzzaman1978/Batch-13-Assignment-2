4a2be317d76f7f4297ce12209b58c9efe22de5cf
Rasheduzzaman <rasheduzzaman1978@gmail.com>
2026-01-19 07:54:08 +0600

    Header section and it's styled modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "576px",  // Small device ends at 576px
+      md: "992px",  // Medium ends at 992px
+      lg: "992px",  // Large starts at 992px (use lg: for large layout)
+      xl: "1200px",
+      "2xl": "1400px",
+    },
     extend: {},
   },
   plugins: [],
